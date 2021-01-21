26df16ddf9a5de3f169b2bd6be92f98950534328
smaxxims <60351309+smaxxims@users.noreply.github.com>
2021-01-21 07:25:07 +0100

    make function for random numbers as string with phpunit methodtests

--- tests/Test.php
+++ tests/Test.php
@@ -7,16 +7,44 @@ include "../models/Cd.php";
 
 class Test extends TestCase
 {
-    function testAddition()
+
+    function testIsCount()
     {
         $test = new Tests();
-        $result = $test->summe(3, 2);
-        $this->assertEquals(5, $result);
+        $result = $test->makeRandomNumsInStringOfNum(7);
+        $this->assertGreaterThanOrEqual(strlen($result), 7);
+        echo "complete: testIsCount";
+    }
+    function testIsString()
+    {
+        $test = new Tests();
+        $result = $test->makeRandomNumsInStringOfNum(5);
+        $this->assertIsString($result);
+        echo "complete: testIsString";
+    }
+    function testIsArray()
+    {
+        $test = new Tests();
+        $result = $test->makeRandomNumsInStringOfNum(5);
+        $this->assertIsArray($result);
+        echo "complete: testIsArray";
+    }
+    function testIsRandom()
+    {
+        $test = new Tests();
+        $result = $test->makeRandomNumsInStringOfNum(5);
+        $this->assertEquals($result, 5);
+        echo "complete: testIsRandom";
+    }
+    function testIsInt()
+    {
+        $test = new Tests();
+        $result = $test->makeRandomNumsInStringOfNum(5);
+        $this->assertIsInt($result);
     }
 
     function testDirExist()
     {
-
         $result = "../public/";
         $this->assertDirectoryExists($result);
         echo "complete: testDirExist";
@@ -37,4 +65,6 @@ class Test extends TestCase
         $this->assertEquals("noimage.jpg", $result);
         echo "complete: testNoImage: $result";
     }
+
+
 }
--- tests/Tests.php
+++ tests/Tests.php
@@ -2,12 +2,18 @@
 
 class Tests
 {
-    public function summe($a, $b)
-    {
-        return $a + $b;
-    }
+
     public function isDirFun($dir)
     {
         return is_dir($dir);
+    }
+
+    public function makeRandomNumsInStringOfNum(int $num): string
+    {
+        $string = "";
+        for ($i = 0; $i < $num; $i++) {
+            $string = $string.random_int(0, 9);
+        }
+        return $string;
     }
 }
