86157066886515bdc3b26e0a528df62d0a113251
smaxxims <60351309+smaxxims@users.noreply.github.com>
2021-01-22 19:03:28 +0100

    test if audio dir would be delete after push

--- controller/Controller.php
+++ controller/Controller.php
@@ -26,7 +26,7 @@ class Controller {
 
         $files = array_diff(scandir($dir), array('.', '..'));
         foreach ($files as $file) {
-            (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+            (is_dir("$dir/$file")) ? $this->deleteAudioDirAndFilesIn("$dir/$file") : unlink("$dir/$file");
         }
         return rmdir($dir);
     }
